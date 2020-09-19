774bdbed815c71a2fbecb71072a30c9d87d95359
Gannon Condon <gcondon@sandiego.edu>
2020-09-19 13:13:38 -0700

    testing commit using R Studio

--- Testing commit.R
+++ Testing commit.R
@@ -1,3 +1,9 @@
+git config --global user.email "gcondon@sandiego.edu"
+git config --global user.name "Gannon Condon"
+
 print("This file was created within RStudio") 
 
 print("And now it lives on GitHub") 
+
+##New code
+v <- c(1:20)
