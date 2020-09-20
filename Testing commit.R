adc51c3e8128192e73a24629935e218f5547387d
Gannon Condon <gcondon@sandiego.edu>
2020-09-19 19:25:06 -0700

    First commit using git bash

--- Testing commit.R
+++ Testing commit.R
@@ -4,6 +4,11 @@ git config --global user.name "Gannon Condon"
 print("This file was created within RStudio") 
 
 print("And now it lives on GitHub") 
-
-##New code
-v <- c(1:20)
+# 
+# cd -- navigate to directory/file
+# git init -- initialize directory
+# git remote add origin <github repo link>
+# git remote -v --checks if it works
+# git add . --select all files  or git add "file name" 
+# git commit -m "comment"
+# git push origin master
